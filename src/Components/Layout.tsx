473d4a5eb3ddc13b4b521f10c7bff16bd3d424be
alihaghjou <alihaghjou30@gmail.com>
2023-09-08 13:14:47 +0330

    footer added

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,12 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { Facebook, Twitter, Instagram, Linkedin } from "react-feather";
 
 export default function Layout() {
   return (
     <main className="p-2 mb-3 min-h-screen text-slate-100 bg-gradient-to-l from-[#2196f3] to-[#f44336]">
       <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-white to-slate-300 font-semibold text-2xl text-center border-b p-4">
-        text that is color
+        Get The Latest News
       </h1>
       <Outlet />
+      <footer className="flex flex-col border-t border-t-cyan-500 p-3">
+        <section className="flex justify-center gap-4 text-lg">
+          <Link to="/">
+            <button className="flex items-center hover:text-red-300"><Facebook />Facebook</button>
+          </Link>
+          <Link to="/">
+            <button className="flex items-center hover:text-red-300"><Twitter />Twitter</button>
+          </Link>
+          <Link to="/">
+            <button className="flex items-center hover:text-red-300"><Instagram />Instagram</button>
+          </Link>
+          <Link to="/">
+            <button className="flex items-center hover:text-red-300"><Linkedin />LinkTree</button>
+          </Link>
+        </section>
+        <h2 className="text-center mt-3">Copyright 2022</h2>
+      </footer>
     </main>
   );
 }
--- src/Components/MainPageDisplay.tsx
+++ src/Components/MainPageDisplay.tsx
@@ -13,7 +13,7 @@ export default function MainPageDisplay({ data, setFilter }: propsType) {
     <main>
       <h1 className="text-lg font-semibold text-center py-2">First 100 News</h1>
       <section className="flex p-3 flex-col-reverse lg:flex-row">
-        <section className="flex flex-col gap-4 p-2 text-lg w-4/5 ">
+        <article className="flex flex-col gap-4 p-2 text-lg w-4/5 ">
           {data?.map((article, i) => (
             <article key={i}>
               <Link
@@ -24,13 +24,18 @@ export default function MainPageDisplay({ data, setFilter }: propsType) {
               </Link>
             </article>
           ))}
-        </section>
+        </article>
         <section className="lg:w-1/5 h-full text-lg border-l text-center p-4 px-6 shadow-sm rounded">
           <p className="mb-3 text-red-300">Get Latest News Of:</p>
           <ul className="indent-4 flex flex-row gap-2 lg:flex-col justify-center">
             {filterBased.map((name) => (
               <li key={name}>
-                <button onClick={() => setFilter(name)} className="capitalize hover:text-orange-300">{name}</button>
+                <button
+                  onClick={() => setFilter(name)}
+                  className="capitalize hover:text-orange-300"
+                >
+                  {name}
+                </button>
               </li>
             ))}
           </ul>
