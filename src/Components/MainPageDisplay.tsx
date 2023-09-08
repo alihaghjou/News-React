ee5d6417fd579042a42db06b2d2e568a47a9a70a
alihaghjou <alihaghjou30@gmail.com>
2023-09-08 11:27:04 +0330

    did a really great and efficent thing

--- src/Components/MainPageDisplay.tsx
+++ src/Components/MainPageDisplay.tsx
@@ -6,39 +6,35 @@ type propsType = {
   setFilter: React.Dispatch<React.SetStateAction<string>>;
 };
 
+const filterBased = ["bitcoin", "apple", "playstation", "car", "xbox"];
+
 export default function MainPageDisplay({ data, setFilter }: propsType) {
   return (
-    <main className="flex p-3">
-      <section className="flex flex-col gap-4 p-2 text-lg w-4/5 ">
-        {data?.map((article, i) => (
-          <article key={i}>
-            <Link
-              to={`/${i}`}
-              className="transition-all text-white duration-200 hover:text-slate-700"
-            >
-              {article.title}
-            </Link>
-          </article>
-        ))}
-      </section>
-      <section className="w-1/5 text-lg fixed right-0 top-1/2 -translate-y-1/2 border-l p-4 px-6 shadow-sm rounded">
-        <p className="mb-3">Get Latest News Of:</p>
-        <ul className="indent-4 flex flex-col gap-2">
-          <li>
-            <button onClick={() => setFilter("bitcoin")}>Bitcoin</button>
-          </li>
-          <li>
-            <button onClick={() => setFilter("apple")}>Apple</button>
-          </li>
-          <li>
-            <button onClick={() => setFilter("playstation")}>
-              Playstation
-            </button>
-          </li>
-          <li>
-            <button onClick={() => setFilter("car")}>Car</button>
-          </li>
-        </ul>
+    <main>
+      <h1 className="text-lg font-semibold text-center py-2">First 100 News</h1>
+      <section className="flex p-3 flex-col-reverse lg:flex-row">
+        <section className="flex flex-col gap-4 p-2 text-lg w-4/5 ">
+          {data?.map((article, i) => (
+            <article key={i}>
+              <Link
+                to={`/${i}`}
+                className="transition-all line-clamp-2 text-white duration-200 hover:text-slate-700"
+              >
+                {article.title}
+              </Link>
+            </article>
+          ))}
+        </section>
+        <section className="lg:w-1/5 h-full text-lg border-l text-center p-4 px-6 shadow-sm rounded">
+          <p className="mb-3 text-red-300">Get Latest News Of:</p>
+          <ul className="indent-4 flex flex-row gap-2 lg:flex-col justify-center">
+            {filterBased.map((name) => (
+              <li key={name}>
+                <button onClick={() => setFilter(name)} className="capitalize hover:text-orange-300">{name}</button>
+              </li>
+            ))}
+          </ul>
+        </section>
       </section>
     </main>
   );
