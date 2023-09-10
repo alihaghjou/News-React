c4ead6397ac1d347ebdbdd0f5ffa2fa067acdfec
alihaghjou <alihaghjou30@gmail.com>
2023-09-10 10:15:35 +0330

    each news page data added

--- src/routes/EachNewsPage.tsx
+++ src/routes/EachNewsPage.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useNews } from "../utils/getData";
+import EachNewsDisplay from "../Components/EachNewsDisplay";
 
 export default function EachNewsPage({filter}: {filter: string}) {
   const { data, isLoading, error } = useNews(filter);
@@ -8,5 +9,5 @@ export default function EachNewsPage({filter}: {filter: string}) {
   if (isLoading) return <div>Loading...</div>;
   if (error || !data || !id) return <div>{error.message}</div>;
   const pageData = data[+id];
-  return <div>{pageData.title}</div>;
+  return <EachNewsDisplay pageData={pageData} />;
 }
